7dda4e8f81e6ae8b42511cc082e9a022423ee8e5
NTKhang <nthanhkhang2003@gmail.com>
2022-06-30 11:56:57 +0700

    add threadTheme key

--- src/getThreadList.js
+++ src/getThreadList.js
@@ -9,7 +9,7 @@ function createProfileUrl(url, username, id) {
 }
 
 function formatParticipants(participants) {
-  return participants.edges.map((p)=>{
+  return participants.edges.map((p) => {
     p = p.node.messaging_actor;
     switch (p["__typename"]) {
       case "User":
@@ -21,7 +21,7 @@ function formatParticipants(participants) {
           gender: p.gender,
           url: p.url, // how about making it profileURL
           profilePicture: p.big_image_src.uri,
-          username: (p.username||null),
+          username: (p.username || null),
           // TODO: maybe better names for these?
           isViewerFriend: p.is_viewer_friend, // true/false
           isMessengerUser: p.is_messenger_user, // true/false
@@ -37,13 +37,13 @@ function formatParticipants(participants) {
           name: p.name,
           url: p.url,
           profilePicture: p.big_image_src.uri,
-          username: (p.username||null),
+          username: (p.username || null),
           // uhm... better names maybe?
           acceptsMessengerUserFeedback: p.accepts_messenger_user_feedback, // true/false
           isMessengerUser: p.is_messenger_user, // true/false
           isVerified: p.is_verified, // true/false
           isMessengerPlatformBot: p.is_messenger_platform_bot, // true/false
-          isMessageBlockedByViewer: p.is_message_blocked_by_viewer, // true/false
+          isMessageBlockedByViewer: p.is_message_blocked_by_viewer // true/false
         };
       case "ReducedMessagingActor":
         return {
@@ -52,8 +52,8 @@ function formatParticipants(participants) {
           name: p.name,
           url: createProfileUrl(p.url, p.username, p.id), // in this case p.url is null all the time
           profilePicture: p.big_image_src.uri, // in this case it is default facebook photo, we could determine gender using it
-          username: (p.username||null), // maybe we could use it to generate profile URL?
-          isMessageBlockedByViewer: p.is_message_blocked_by_viewer, // true/false
+          username: (p.username || null), // maybe we could use it to generate profile URL?
+          isMessageBlockedByViewer: p.is_message_blocked_by_viewer // true/false
         };
       case "UnavailableMessagingActor":
         return {
@@ -62,15 +62,15 @@ function formatParticipants(participants) {
           name: p.name, // "Facebook User" in user's language
           url: createProfileUrl(p.url, p.username, p.id), // in this case p.url is null all the time
           profilePicture: p.big_image_src.uri, // default male facebook photo
-          username: (p.username||null), // maybe we could use it to generate profile URL?
-          isMessageBlockedByViewer: p.is_message_blocked_by_viewer, // true/false
+          username: (p.username || null), // maybe we could use it to generate profile URL?
+          isMessageBlockedByViewer: p.is_message_blocked_by_viewer // true/false
         };
       default:
         log.warn("getThreadList", "Found participant with unsupported typename. Please open an issue at https://github.com/Schmavery/fca-unofficial/issues\n" + JSON.stringify(p, null, 2));
         return {
           accountType: p["__typename"],
           userID: utils.formatID(p.id.toString()),
-          name: p.name || `[unknown ${p["__typename"]}]`, // probably it will always be something... but fallback to [unknown], just in case
+          name: p.name || `[unknown ${p["__typename"]}]` // probably it will always be something... but fallback to [unknown], just in case
         };
     }
   });
@@ -87,32 +87,33 @@ function formatColor(color) {
 function getThreadName(t) {
   if (t.name || t.thread_key.thread_fbid) return t.name;
 
-  for (let po of t.all_participants.edges) {
-    let p = po.node;
+  for (const po of t.all_participants.edges) {
+    const p = po.node;
     if (p.messaging_actor.id === t.thread_key.other_user_id) return p.messaging_actor.name;
   }
 }
 
 function mapNicknames(customizationInfo) {
   return (customizationInfo && customizationInfo.participant_customizations) ? customizationInfo.participant_customizations.map(u => {
-      return {
-        "userID": u.participant_id,
-        "nickname": u.nickname
-      };
-  }):[];
+    return {
+      "userID": u.participant_id,
+      "nickname": u.nickname
+    };
+  }) : [];
 }
 
 function formatThreadList(data) {
   return data.map(t => {
-    let lastMessageNode = (t.last_message&&t.last_message.nodes&&t.last_message.nodes.length>0)?t.last_message.nodes[0]:null;
+    const lastMessageNode = (t.last_message && t.last_message.nodes && t.last_message.nodes.length > 0) ? t.last_message.nodes[0] : null;
     return {
-      threadID: t.thread_key?utils.formatID(t.thread_key.thread_fbid || t.thread_key.other_user_id):null, // shall never be null
+      threadID: t.thread_key ? utils.formatID(t.thread_key.thread_fbid || t.thread_key.other_user_id) : null, // shall never be null
       name: getThreadName(t),
       unreadCount: t.unread_count,
       messageCount: t.messages_count,
-      imageSrc: t.image?t.image.uri:null,
-      emoji: t.customization_info?t.customization_info.emoji:null,
-      color: formatColor(t.customization_info?t.customization_info.outgoing_bubble_color:null),
+      imageSrc: t.image ? t.image.uri : null,
+      emoji: t.customization_info ? t.customization_info.emoji : null,
+      color: formatColor(t.customization_info ? t.customization_info.outgoing_bubble_color : null),
+      threadTheme: t.thread_theme,
       nicknames: mapNicknames(t.customization_info),
       muteUntil: t.mute_until,
       participants: formatParticipants(t.all_participants),
@@ -123,7 +124,7 @@ function formatThreadList(data) {
       // isPinProtected: t.is_pin_protected, // feature from future? always false (2018-04-04)
       customizationEnabled: t.customization_enabled, // false for ONE_TO_ONE with Page or ReducedMessagingActor
       participantAddMode: t.participant_add_mode_as_string, // "ADD" if "GROUP" and null if "ONE_TO_ONE"
-      montageThread: t.montage_thread?Buffer.from(t.montage_thread.id,"base64").toString():null, // base64 encoded string "message_thread:0000000000000000"
+      montageThread: t.montage_thread ? Buffer.from(t.montage_thread.id, "base64").toString() : null, // base64 encoded string "message_thread:0000000000000000"
       // it is not userID nor any other ID known to me...
       // can somebody inspect it? where is it used?
       // probably Messenger Day uses it
@@ -134,13 +135,13 @@ function formatThreadList(data) {
       timestamp: t.updated_time_precise, // in miliseconds
       // isCanonicalUser: t.is_canonical_neo_user, // is it always false?
       // TODO: how about putting snippet in another object? current implementation does not handle every possibile message type etc.
-      snippet: lastMessageNode?lastMessageNode.snippet:null,
-      snippetAttachments: lastMessageNode?lastMessageNode.extensible_attachment:null, // TODO: not sure if it works
-      snippetSender: lastMessageNode?utils.formatID((lastMessageNode.message_sender.messaging_actor.id || "").toString()):null,
-      lastMessageTimestamp: lastMessageNode?lastMessageNode.timestamp_precise:null, // timestamp in miliseconds
-      lastReadTimestamp: (t.last_read_receipt&&t.last_read_receipt.nodes.length>0)
-                         ? (t.last_read_receipt.nodes[0]?t.last_read_receipt.nodes[0].timestamp_precise:null)
-                         : null, // timestamp in miliseconds
+      snippet: lastMessageNode ? lastMessageNode.snippet : null,
+      snippetAttachments: lastMessageNode ? lastMessageNode.extensible_attachment : null, // TODO: not sure if it works
+      snippetSender: lastMessageNode ? utils.formatID((lastMessageNode.message_sender.messaging_actor.id || "").toString()) : null,
+      lastMessageTimestamp: lastMessageNode ? lastMessageNode.timestamp_precise : null, // timestamp in miliseconds
+      lastReadTimestamp: (t.last_read_receipt && t.last_read_receipt.nodes.length > 0)
+        ? (t.last_read_receipt.nodes[0] ? t.last_read_receipt.nodes[0].timestamp_precise : null)
+        : null, // timestamp in miliseconds
       cannotReplyReason: t.cannot_reply_reason, // TODO: inspect possible values
       approvalMode: Boolean(t.approval_mode),
 
@@ -151,28 +152,28 @@ function formatThreadList(data) {
   });
 }
 
-module.exports = function(defaultFuncs, api, ctx) {
+module.exports = function (defaultFuncs, api, ctx) {
   return function getThreadList(limit, timestamp, tags, callback) {
     if (!callback && (utils.getType(tags) === "Function" || utils.getType(tags) === "AsyncFunction")) {
       callback = tags;
       tags = [""];
     }
     if (utils.getType(limit) !== "Number" || !Number.isInteger(limit) || limit <= 0) {
-      throw {error: "getThreadList: limit must be a positive integer"};
+      throw { error: "getThreadList: limit must be a positive integer" };
     }
     if (utils.getType(timestamp) !== "Null" &&
-       (utils.getType(timestamp) !== "Number" || !Number.isInteger(timestamp))) {
-      throw {error: "getThreadList: timestamp must be an integer or null"};
+      (utils.getType(timestamp) !== "Number" || !Number.isInteger(timestamp))) {
+      throw { error: "getThreadList: timestamp must be an integer or null" };
     }
     if (utils.getType(tags) === "String") {
       tags = [tags];
     }
     if (utils.getType(tags) !== "Array") {
-      throw {error: "getThreadList: tags must be an array"};
+      throw { error: "getThreadList: tags must be an array" };
     }
 
-    var resolveFunc = function(){};
-    var rejectFunc = function(){};
+    var resolveFunc = function () { };
+    var rejectFunc = function () { };
     var returnPromise = new Promise(function (resolve, reject) {
       resolveFunc = resolve;
       rejectFunc = reject;
@@ -194,7 +195,7 @@ module.exports = function(defaultFuncs, api, ctx) {
           // This doc_id was valid on 2020-07-20
           "doc_id": "3336396659757871",
           "query_params": {
-            "limit": limit+(timestamp?1:0),
+            "limit": limit + (timestamp ? 1 : 0),
             "before": timestamp,
             "tags": tags,
             "includeDeliveryReceipts": true,
@@ -214,7 +215,7 @@ module.exports = function(defaultFuncs, api, ctx) {
         }
 
         if (resData[resData.length - 1].successful_results === 0) {
-          throw {error: "getThreadList: there was no successful_results", res: resData};
+          throw { error: "getThreadList: there was no successful_results", res: resData };
         }
 
         // When we ask for threads using timestamp from the previous request,
