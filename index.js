e33d0ce3cb26fdcabef2f3daefdd2fee366e4b4a
tung <tung@phamthanh.me>
2022-10-20 22:51:59 +0700

    fix new quote format for legacyFBMQTT regex match

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function buildAPI(globalOptions, html, jar) {
       region = new URL(mqttEndpoint).searchParams.get("region").toUpperCase();
       log.info("login", `Got this account's message region: ${region}`);
     } else {
-      let legacyFBMQTTMatch = html.match(/(\["MqttWebConfig",\[\],{fbid:")(.+?)(",appID:219994525426954,endpoint:")(.+?)(",pollingEndpoint:")(.+?)(3790])/);
+      let legacyFBMQTTMatch = html.match(/(\["MqttWebConfig",\[\],{"?fbid"?:")(.+?)(","?appID"?:219994525426954,"?endpoint"?:")(.+?)(","?pollingEndpoint"?:")(.+?)(3790])/);
       if (legacyFBMQTTMatch) {
         mqttEndpoint = legacyFBMQTTMatch[4];
         region = new URL(mqttEndpoint).searchParams.get("region").toUpperCase();
